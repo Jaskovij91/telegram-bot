11bfb451397b15ea606c7c78b54df8eb60a0b60a
Jaskovij91 <151053391+Jaskovij91@users.noreply.github.com>
2023-11-18 22:53:38 +0500

    Create dars1.py

---
+++ dars1.py
@@ -0,0 +1,53 @@
+# 01- dars. Print() funksiyasi endi o'rganayotgan paytim
+# sana 03.11.2023
+# Muallif: Jasurbek Poziljonov
+
+
+# Quyidagi kodlar jumlalarni konsolga chiqaradi 
+# print('Assalomu Alaykum')
+# print(Hayrli Tong) # Bu noto'g'ri kod
+# print("Hayrli Tong) # Bu noto'g'ri kod
+# print(Hayrli Tong! # Bu noto'g'ri kod
+# print("Hayrli Tong") # Bu noto'g'ri kod qo'shtirnoq va bir tirnoq
+# print('hayrli Tong') # bu esa to'g'ri kod
+# print('Men \"Dell"\ markasidagi noutbuk sotib oldim') # bu to'g'ri kod \ siz bir hil matnni ichida "" qilib bo'lmaydi va '' ham bo'lmaydi
+# print('Men "Microsoft" kompaniyasiga ishga kirdim')
+# print('Men hozirda "PYTHON" dasturlash tilini o\'rganmoqdaman') # \ bu belgi o' g' harflarida ham qatnashadi agar qatnashtirmoqchi bo'lmasak '' ichiga olinadi
+# print('Men hozirda "PYTHON" dasturlash tilini o\'rganmoqdaman, bu dastur menga juda ham yoqdi')
+#print('''Men hozirda "PYTHON" dasturlash tilini o\'rganmoqdaman, 
+# Bu dastur, menga juda ham yoqdi''') # matnni 2 ga bo'lish usuli 
+print('Men hozirda "PYTHON" \nDasturlash tilini o\'rganmoqdaman, \nBu dastur menga juda ham yoqdi, \nLekin hali ham? \nO\'sha gapda')
+
+
+
+# print('Men "Dell" markasidagi noutbuk sotib oldim')   
+# print("""Odami ersang, demagil odami,
+# print("""Odami ersang, demagil odami, Oniki, yo'q xalq g'amidin g'ami""")
+# print("Odami ersang, demagil odami,\nOniki, yo'q xalq g'amidin g'ami")
+# print('Odami ersang, demagil odami, \nOniki, yo\'q xalq g\'amidin g\'ami')
+# print(2+4*2)
+# print(19/5)
+# print(20/5)
+# print(16//4)
+# print(10//3)
+# print(2**4)
+# print("To'qqizning kvadrati", 9**2, "ga teng")
+# print('3x3=',3*3)
+# print(2*5*3.14159)
+#Radiusi 5 ga teng bo'lgan aylananing uzunligi quyidagicha hisoblanadi
+# print(2*5*3.14159)
+# print("Assalom alaykum!") # Bu matn konsolda chiqadi
+#Keyingi qator esa bajarilmaydi
+#print("Mening ismim Jasurbek")
+
+# Keyingi mashqlar 
+#mehmonlar = ('Ahror', 'Sulton', 'Ulug\'bek', 'Bobur', 'Murodxon', 'G\'ofurjon',)
+#for mehmon in mehmonlar: 
+    #print("Salom", mehmon)
+    #print('Yangi navbatdan $ oshga menham hudo hohlasa qo\'shilaman', mehmon)
+    
+
+# mehmonlar = ('Ahror', 'Sulton', 'Ulug\'bek', 'Bobur', 'Murodxon', 'G\'ofurjon',)
+# for mehmon in mehmonlar: 
+    #print(f"Hurmatli {mehmon}, sizni 15 noyabr kuni oshga taklif qilamiz")
+    #print("Hurmat bilan, Group Sinfdoshlar")
