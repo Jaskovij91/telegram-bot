1b1130b69b5c460629c6400374ddb260f47359aa
Jaskovij91 <151053391+Jaskovij91@users.noreply.github.com>
2023-11-18 22:48:15 +0500

    yangi dars
    yangi pythonda o'rgangan darsliklarim qo'shildi

---
+++ temp.py
@@ -0,0 +1,99 @@
+#mehmonlar = ('Ahror', 'Sulton', 'Ulug\'bek', 'Bobur', 'Murodxon', 'G\'ofurjon',)
+#for mehmon in mehmonlar: 
+    #print("Salom", mehmon)
+    #print('Yangi navbatdan $ oshga menham hudo hohlasa qo\'shilaman', mehmon)
+    
+
+# mehmonlar = ('Ahror', 'Sulton', 'Ulug\'bek', 'Bobur', 'Murodxon', 'G\'ofurjon',)
+# for mehmon in mehmonlar: 
+    #print(f"Hurmatli {mehmon}, sizni 15 noyabr kuni oshga taklif qilamiz")
+     #print("Hurmat bilan, Group Sinfdoshlar")
+#print('Men "Dell" markasidagi noutbuk sotib oldim')   
+#ism = 'Jasurbek'
+#print("Mening ismim " + ism)
+#matn = "Men yangi 📱 oldim"
+#print(matn)
+#ism = 'Jasurbek'
+#familiya = 'Poziljonov'
+#print(ism + familiya)
+#ism = 'Jasurbek'
+#familiya = 'poziljonov'
+#print(ism + ' ' + familiya) # ikki o'zgaruvchi orasiga bo'sh joy qo'shamiz
+#ism = "Jasurbek"
+#familiya = 'Poziljonov'
+#ism_sharif = f"{ism} {familiya}"
+#print(ism_sharif)
+#ism = "Jasurbek"
+#familiya = 'Poziljonov'
+#print(f"Salom, mening ismim {ism}. {ism} {familiya}!")
+#print('Hello World!')
+#print('Hello \tWorld!')
+#print('Hello \nWorld!')
+#ism = 'Jasurbek'
+#familiya = 'Poziljonov'
+#ism_sharif = f"{ism} {familiya}"
+#print(ism_sharif.upper()) # upper() metodi matndagi har bir harfni katta harfga o'zgartiradi. 
+#ism = 'Jasurbek'
+#familiya = 'Poziljonov'
+#ism_sharif = f"{ism} {familiya}"
+#print(ism_sharif.lower()) # lower() metodi esa aksincha, har bir harfni kichik harfga o'zgartiradi.
+#ism_sharif = 'jasurbek poziljonov'
+#print(ism_sharif.title()) # title() metodi matndagi har bir so'zning birinchi harfini katta harf bilan yozadi. 
+#ism_sharif = 'jasurbek poziljonov'
+#print(ism_sharif.capitalize()) # capitalize() esa faqatgina eng birinchi so'zning birinchi harfini katta bilan yozadi.
+#print('james bond'.upper()) # Metodlarni faqat o'zgaruvchilarga emas, balki to'g'ridan-to'g'ri matnga ham qo'llash mumkin (aslida o'zgaruvchi ham matnning (yoki boshqa ma'lumotning) manzili xolos)
+#meva = "     olma     "
+#print("Men " + meva.lstrip() + " yaxshi ko'raman") # lstrip() — matn boshidagi bo'shliqni,
+#print("Men " + meva.rstrip() + " yaxshi ko'raman") # rstrip() – matn oxiridagi bo'shliqni,
+#print("Men " + meva.strip() + " yaxshi ko'raman") # strip() — matn boshi va oxiridagi bo'shliqlarni olib tashlaydi
+#print("Men " + meva + " yaxshi ko'raman") 
+#ism = input("Ismingiz nima?") # Shu paytgacha biz o'zgaruvchilarning qiymatini dasturning ichida berayotgan edik. Keling endi qiymatni o'zimiz emas, balki dastur foydalanuvchilariga kiritish imkonini beramiz.
+#print("Assalom alaykum, " + ism)
+#ism = input("Ismingiz nima?\n>>>") # Foydalanuvchi ismini yangi qatordan kiritadi
+#print("Assalom alaykum, " + ism.title()) # bu keyingi qatordan yozish
+#kocha="Baynalminal ko\'chasi"
+#mahalla="Do\'tlik MFY"
+#tuman="Quva tumani" 
+#viloyat="Farg\'ona viloyati" 
+#manzil = input("ko\'changiz?")
+#print("Assalomu alaykum," + manzil.title())
+#manzil = f"men {kocha} , {mahalla} , {tuman} , {viloyat} " # Ikki (va undan ko'p) matn ko'rinishidagi o'zgaruvchilarni birlashtirish uchun f-string usulidan  f"{matn1} {matn2}" ham foydalansak bo'ladi:
+#print("Iltimos, quyidagi ma'lumotlarni kiriting:")
+#kocha = input("Ko'changiz: ")
+#mahalla = input("Mahallangiz: ")
+#tuman = input("Tumaningiz: ")
+#viloyat = input("Viloyatingiz: ")
+#manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+#print(manzil.upper())
+#print(manzil.lower())
+#print(manzil.title())
+#print(manzil.capitalize())
+#1 foydalanuvchining tug'ilgan yilini so'raymiz
+#t_yil = int(input("Tug'ilgan yilingizni kiriting: ")) 
+#yosh = 2023 - t_yil
+#print("Siz ",  yosh,  " yoshda ekansiz")
+#avtolar = []
+bozorlik = ["yog'", 'un', 'piyoz', 'banan', "go'sht"]
+mahsulot = bozorlik.pop(3) # Ro'yxatdan banan ni sug'urib olamiz
+print("Men " + mahsulot + " sotib oldim")
+print("Olinmagan mahsulotlar: ", bozorlik)  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
